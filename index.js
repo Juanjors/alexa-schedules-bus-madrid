6d7e3dd40346d3d7ad53db1820fb5878991d6cef
Juanjo <juanjoramirezsanchez@gmail.com>
2019-01-29 00:05:18 +0100

    refactor: code variables are now in English. fix: added a workaround to get localTime since the server is deployed in Ireland (if it changes it will break).

--- index.js
+++ index.js
@@ -4,20 +4,25 @@ const Alexa = require('ask-sdk-core');
 const schedules = require('./schedules.js');
 
 const SKILL_NAME = 'horario autobus';
-const NEXT_SCHEDULE = 'El siguiente autobús pasa a las: ';
+const MORE_DATA = '¿Quieres saber mas información sobre los siguientes autobuses?'
+const MORE_DATA_RESPONSE = 'Los siguientes autobuses son a las: ';
+const MORE_DATA_FAIL = 'No tengo datos de autobuses';
 const HELP_MESSAGE = 'Te puedo ayudar a saber cuál será el siguiente autobús';
 const HELP_REPROMPT = '¿Cómo te puedo ayudar?';
 const STOP_MESSAGE = '!Adiós!';
+let next_schedule = 'El siguiente autobús pasa a las: ';
+
+let session = {};
 
 const getSchedules = {
   canHandle(handlerInput) {
     const request = handlerInput.requestEnvelope.request;
-    console.log({ request });
     return request.type === 'LaunchRequest'
       || (request.type === 'IntentRequest'
         && request.intent.name === 'GetHorariosIntent');
   },
   handle(handlerInput) {
+    const timestamp = handlerInput.requestEnvelope.request.timestamp;
     const date = new Date();
     let currentHour = date.getHours() + 1; //Cutre workarond to get the timezone from Spain (Servers are located in Ireland)
     let currentMinute = date.getMinutes();
@@ -28,26 +33,52 @@ const getSchedules = {
         const timeAndMinutes = hora.split(":");
         let timeSchedule = timeAndMinutes[0];
         let minuteSchedule = timeAndMinutes[1];
+        console.log("current", currentHour, "horarios", timeSchedule);
         if (currentHour < timeSchedule || ((currentHour == timeSchedule) && currentMinute < minuteSchedule)) {
           nextBuses.push(`${timeSchedule}:${minuteSchedule}`);
         }
-
       });
-      return nextBuses[0];
+      session.nextBuses = nextBuses.slice();
+      session.nextBuses.pop();
+      handlerInput.attributesManager.setSessionAttributes(session);
+      return nextBuses;
     }
 
     function isWeekend(date) {
       const day = date.getDay();
       return (day === 6) || (day === 0);
     }
+    //if array is empty it means that there are no more buses today
+    const buses = (nextBus.length === 0);
+    if (buses) next_schedule = "No hay más autobuses para el día de hoy";
 
     return handlerInput.responseBuilder
-      .speak(NEXT_SCHEDULE + nextBus)
-      .withSimpleCard(SKILL_NAME, nextBus)
+      .speak(next_schedule + (!buses ? nextBus[0] : '') + MORE_DATA)
+      .reprompt("")
+      .withSimpleCard(SKILL_NAME, nextBus[0])
       .getResponse();
   },
 };
 
+
+const HandleMoreInfo = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    const slots = handlerInput.requestEnvelope.request.intent.slots;
+    console.log("@@@@", slots);
+    return request.type === 'IntentRequest'
+      && request.intent.name === 'AMAZON.YesIntent';
+  },
+  handle(handlerInput) {
+    const slots = handlerInput.requestEnvelope.request.intent.slots;
+    let nextBuses = handlerInput.attributesManager.getSessionAttributes();
+
+    console.log("@@@@", slots);
+    return handlerInput.responseBuilder
+      .speak(MORE_DATA_RESPONSE) //nextBuses ? nextBuses.join(",") : MORE_DATA_FAIL)
+      .getResponse();
+  },
+};
 
 
 const HelpHandler = {
@@ -59,24 +90,10 @@ const HelpHandler = {
   handle(handlerInput) {
     return handlerInput.responseBuilder
       .speak(HELP_MESSAGE)
-      .reprompt(HELP_REPROMPT)
       .getResponse();
   },
 };
 
-const FallbackHandler = {
-  canHandle(handlerInput) {
-    const request = handlerInput.requestEnvelope.request;
-    return request.type === 'IntentRequest'
-      && request.intent.name === 'AMAZON.FallbackIntent';
-  },
-  handle(handlerInput) {
-    return handlerInput.responseBuilder
-      .speak(HELP_MESSAGE)
-      .reprompt(HELP_MESSAGE)
-      .getResponse();
-  },
-};
 
 const ExitHandler = {
   canHandle(handlerInput) {
@@ -125,7 +142,7 @@ exports.handler = skillBuilder
     getSchedules,
     HelpHandler,
     ExitHandler,
-    FallbackHandler,
+    HandleMoreInfo,
     SessionEndedRequestHandler
   )
   .addErrorHandlers(ErrorHandler)
